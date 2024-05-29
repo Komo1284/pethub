26f07e7cc8e27ab5123cd0a01d950e83836bb7d0
K-solar <ehdduf1126@naver.com>
2024-05-29 20:28:06 +0900

    first commit

--- src/main/java/itbank/pethub/vo/MemberVO.java
+++ src/main/java/itbank/pethub/vo/MemberVO.java
@@ -1,4 +1,15 @@
 package itbank.pethub.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MemberVO {
+
+    private int id;
+    private String name, address, email, phone;
+    private String userid, userpw, newpw, nick;
+
+
 }
