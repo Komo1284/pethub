d257629256a47e14c70dfcfd08a8d2c35e8d3c14
K-solar <ehdduf1126@naver.com>
2024-05-29 21:48:36 +0900

    second member_basic commit

--- src/main/java/itbank/pethub/vo/MemberVO.java
+++ src/main/java/itbank/pethub/vo/MemberVO.java
@@ -2,14 +2,16 @@ package itbank.pethub.vo;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 public class MemberVO {
 
-    private int id;
+    private int id, profile;
     private String name, address, email, phone;
     private String userid, userpw, newpw, nick;
-
+    private String image;
+    private MultipartFile upload;
 
 }
