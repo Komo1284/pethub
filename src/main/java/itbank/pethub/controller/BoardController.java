94b8818550381450eafe20e1ace94ace2c5b5333
xoghks1202@naver.com <xoghks1202@naver.com>
2024-05-31 15:44:05 +0900

    test

--- src/main/java/itbank/pethub/controller/BoardController.java
+++ src/main/java/itbank/pethub/controller/BoardController.java
@@ -1,9 +1,13 @@
 package itbank.pethub.controller;
 
 import itbank.pethub.service.BoardService;
+import itbank.pethub.vo.BoardVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/board")
@@ -11,6 +15,5 @@ public class BoardController {
 
     @Autowired
     private BoardService bs;
-
 
 }
--- src/main/java/itbank/pethub/model/BoardDAO.java
+++ src/main/java/itbank/pethub/model/BoardDAO.java
@@ -1,7 +1,13 @@
 package itbank.pethub.model;
 
+import itbank.pethub.vo.BoardVO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface BoardDAO {
+
 }
