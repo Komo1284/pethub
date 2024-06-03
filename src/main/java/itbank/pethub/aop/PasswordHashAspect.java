4c7af16fc5a134d8f42856f879035d53a2299682
Komorebi <karan1284@naver.com>
2024-06-04 04:37:06 +0900

    refactoring aop

--- src/main/java/itbank/pethub/aop/PasswordHashAspect.java
+++ src/main/java/itbank/pethub/aop/PasswordHashAspect.java
@@ -9,23 +9,11 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class PasswordHashAspect {
 
-    @Before("execution(* itbank.pethub.service.MemberService.login(..)) && args(input)")
-    public void hashPasswordBeforeLogin(MemberVO input) {
-        input.setUserpw(PasswordEncoder.encode(input.getUserpw()));
-    }
-
-    @Before("execution(* itbank.pethub.service.MemberService.signUp(..)) && args(input)")
-    public void hashPasswordBeforeSignUp(MemberVO input) {
-        input.setUserpw(PasswordEncoder.encode(input.getUserpw()));
-    }
-
-    @Before("execution(* itbank.pethub.service.MemberService.update(..)) && args(input)")
-    public void hashPasswordBeforeUpdate(MemberVO input) {
-        input.setUserpw(PasswordEncoder.encode(input.getUserpw()));
-    }
-
-    @Before("execution(* itbank.pethub.service.MemberService.updateNoProfile(..)) && args(input)")
-    public void hashPasswordBeforeUpdateNoProfile(MemberVO input) {
+    @Before("execution(* itbank.pethub.service.MemberService.login(..)) || " +
+            "execution(* itbank.pethub.service.MemberService.signUp(..)) || " +
+            "execution(* itbank.pethub.service.MemberService.update(..)) || " +
+            "execution(* itbank.pethub.service.MemberService.updateNoProfile(..)) && args(input)")
+    public void hashPasswordBeforeAnyMethod(MemberVO input) {
         input.setUserpw(PasswordEncoder.encode(input.getUserpw()));
     }
 }
