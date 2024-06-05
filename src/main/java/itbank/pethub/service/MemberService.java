d0e6d5eeb24baef98611e3245f80df0576b816da
Eastten <ehdduf1126@naver.com>
2024-06-05 21:29:07 +0900

    commit first jwt

--- src/main/java/itbank/pethub/service/MemberService.java
+++ src/main/java/itbank/pethub/service/MemberService.java
@@ -1,10 +1,15 @@
 package itbank.pethub.service;
 
 import itbank.pethub.aop.PasswordEncoder;
+import itbank.pethub.dto.MemberDetails;
 import itbank.pethub.model.MemberDAO;
+import itbank.pethub.repository.MemberRepository;
 import itbank.pethub.vo.CouponVO;
 import itbank.pethub.vo.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
