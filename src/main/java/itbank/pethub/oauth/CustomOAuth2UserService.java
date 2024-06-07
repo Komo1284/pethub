3d6c768c6820c9caec55690642a830cb9ff0cddf
Eastten <ehdduf1126@naver.com>
2024-06-07 16:53:33 +0900

    commit third jwtdevelop

--- src/main/java/itbank/pethub/oauth/CustomOAuth2UserService.java
+++ src/main/java/itbank/pethub/oauth/CustomOAuth2UserService.java
@@ -4,12 +4,16 @@ import itbank.pethub.model.MemberDAO;
 import itbank.pethub.vo.MemberVO;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
