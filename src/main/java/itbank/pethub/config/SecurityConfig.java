afac1900c6ca2492d6662fd5e53c273ea114cbe5
Eastten <ehdduf1126@naver.com>
2024-06-12 17:08:45 +0900

    jwtdevleopmodifying commit 2

--- src/main/java/itbank/pethub/config/SecurityConfig.java
+++ src/main/java/itbank/pethub/config/SecurityConfig.java
@@ -4,33 +4,43 @@ package itbank.pethub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 ;
 
 @Configuration
+@EnableWebSecurity
   // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity` http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/user/**").authenticated()
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(oauth -> oauth
+                .requestMatchers("/user/**").authenticated()
                 // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
                 // hasRole('ROLE_USER')")
                 // .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
                 // hasRole('ROLE_USER')")
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .requestMatchers("/admin/**").hasRole("ROLE_ADMIN")
                 .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login")
+                .loginPage("/member/login")
                 .loginProcessingUrl("/loginProc")
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
                 .loginPage("/login")
                 .userInfoEndpoint()
-                .userService(principalOauth2UserService);
+                .userService(principalOauth2UserService));
 
         return http.build();
     }
