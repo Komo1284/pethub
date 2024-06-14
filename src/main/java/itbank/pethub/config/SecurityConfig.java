ac0c44a8ca5430e46b157d5e7c2fc8b9cac81e58
Eastten <ehdduf1126@naver.com>
2024-06-14 16:20:04 +0900

    jwtdevelop real loop problem commit

--- src/main/java/itbank/pethub/config/SecurityConfig.java
+++ src/main/java/itbank/pethub/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import itbank.pethub.jwt.JwtAuthenticationFilter;
 import itbank.pethub.jwt.JwtAuthorizationFilter;
 import itbank.pethub.jwt.JwtProvider;
 import itbank.pethub.model.UserDAO;
+import jakarta.servlet.DispatcherType;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,21 +65,20 @@ public class SecurityConfig {
 
         http.csrf(AbstractHttpConfigurer::disable)
                 .addFilter(corsConfig.corsFilter())
-                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterAt(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(new JwtAuthorizationFilter(authenticationManager(authenticationConfiguration), userDAO), JwtAuthenticationFilter.class)
                 .formLogin(formLogin -> formLogin
                         .loginPage("/member/login")
                         .usernameParameter("userid")
                         .passwordParameter("userpw")
-                        .defaultSuccessUrl("/"))
+                        .defaultSuccessUrl("/", true)
+                        .permitAll())
                 .logout(logout -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
                         .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true))
+                        .invalidateHttpSession(true)
+                        .permitAll())
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/reissue").permitAll()
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
                         .requestMatchers("/","/member/login", "/member/signUp", "member/logout"
                                 , "member/myPage", "member/memberUpdate").permitAll()
                         .requestMatchers(antMatcher("/api/v1/user/**")).hasRole("USER")
@@ -86,19 +86,14 @@ public class SecurityConfig {
                         .requestMatchers(antMatcher("/h2-console/**")).permitAll()
                         .anyRequest().authenticated()
                 )
+//                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2Login(configure ->
                         configure
-                                .loginPage("/member/login")
-                                // sns 로그인이 완료된 후의 후처리가 필요함
-                                // 1. 코드 받기
-                                // 2. 엑세스토큰(권한)
-                                // 3. 사용자 프로필 정보를 가져오고
-                                // 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
-                                // 4-2. (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> (집주소 필요)
-
                                 .userInfoEndpoint(config -> config
                                         .userService(principalOauth2UserService))
-                );
+                )
+                .addFilterAt(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(new JwtAuthorizationFilter(authenticationManager(authenticationConfiguration), userDAO), JwtAuthenticationFilter.class);
 
         return http.build();
     }
